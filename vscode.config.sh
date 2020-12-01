e0802872a6bad2e9051fe0eb5c9b0fafbde0858a
Youssef Noam <ynoam@e1r8p2.1337.ma>
2020-12-01 18:05:23 +0100

    add vscode configuration

---
+++ vscode.config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false         # For VS Code
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false # For VS Code Insider
+defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false    # For VS Codium
+defaults delete -g ApplePressAndHoldEnabled     
